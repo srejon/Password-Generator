19455078bee7ef864acdfdc1d9125e5b7739f8c0
Srejon Sharma <srejonboss2@gmail.com>
2025-03-28 14:59:01 +0600

    changes

---
+++ script.js
@@ -0,0 +1,15 @@
+const passwordField = document.getElementById('password');
+const passLength = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*()_+?/>~-{}";
+
+function createPass() {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+}
