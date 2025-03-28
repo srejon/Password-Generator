b8ee6de8050d3165a59f9798f6b76b61e9a9c52b
Srejon Sharma <srejonboss2@gmail.com>
2025-03-28 15:58:12 +0600

    onclick final

--- script.js
+++ script.js
@@ -6,10 +6,25 @@ const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
 const symbol = "!@#$%^&*()_+?/>~-{}";
 
-function createPass() {
+const allChars = upperCase + lowerCase + number + symbol;
+
+document.getElementById('btn').onclick = function () {
     let password = "";
     password += upperCase[Math.floor(Math.random() * upperCase.length)];
     password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
     password += number[Math.floor(Math.random() * number.length)];
     password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(passLength > password.length)
+        {
+            password += allChars[Math.floor(Math.random() * allChars.length)];
+
+        }
+
+    passwordField.value = password;
+}
+
+document.getElementById('img').onclick = function(){
+    passwordField.select();
+    document.execCommand("copy");
 }
